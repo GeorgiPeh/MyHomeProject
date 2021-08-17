2b44fd3a20d057608c582b39128b7348c5dadc24
GeorgiPeh <georgi.zh.pehlivanov@gmail.com>
2021-08-17 21:27:56 +0300

    Initial commit

---
+++ src/main/java/com/example/myhomeproject/MyhomeprojectApplication.java
@@ -0,0 +1,15 @@
+package com.example.myhomeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeprojectApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(MyhomeprojectApplication.class, args);
+  }
+
+}
